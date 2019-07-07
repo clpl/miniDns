9f7b89415c6c711256e8412f532473685d7be730
clpl <825832365@qq.com>
2019-07-07 15:49:16 +0800

    update

--- nameServer.py
+++ nameServer.py
@@ -12,7 +12,7 @@ class nameServer(socketserver.BaseRequestHandler):
 		sock = self.request[1]
 		print(self.client_address)
 		parseResult = self.__parseData(data)
-		header, query_list = parseResult['header'], parseResult['query_list']
+		header, query_list, query_info = parseResult['header'], parseResult['query_list'], parseResult['query_info']
 		print(query_list)
 		for item in query_list:
 			name, q_type, q_class = item['url'], item['q_type'], item['q_class']
@@ -26,24 +26,20 @@ class nameServer(socketserver.BaseRequestHandler):
 					print('ip = 0.0.0.0')
 				else:
 					print('return answers', ip)
-					res200 = self.__200Data(header, ip)
+					res200 = self.__200Data(header, ip, query_info)
 					sock.sendto(res200, self.client_address)
 			else:
-				ip = self.requestRemote()
 				print('requestRemote:', name)
 				sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 				sock.sendto(data, ('202.106.0.20', 53))
 				udp_rece = sock.recvfrom(1024)
 				print(udp_rece)
 				self.__parseData(udp_rece[0])
-				
+				ip = self.url2ip[name]
+				res200 = self.__200Data(header, ip, query_info)
+				sock.sendto(res200, self.client_address)
+				sock.close()
 
-		# socket = self.request[1]
-		# print("wrote:",self.client_address[0])
-		# print (data)
-		# sock.sendto(self.request[0].strip(), self.client_address)
-		# self.request[0].strip()
-		#socket.sendto(data.upper(), self.client_address)
 
 	def __404data(self, header):
 		answers = 0
@@ -52,7 +48,7 @@ class nameServer(socketserver.BaseRequestHandler):
 		res = struct.pack('>HHHHHH', header['id'], flags, header['quests'], answers, header['author'], header['addition'])
 		return res
 
-	def __200Data(self, header, ip):
+	def __200Data(self, header, ip, query_info):
 		print('hp:', header, ip)
 		answers = 1
 		# AA:1 opcode:0000 AA:0 TC:0 RD:1 RA:1 zero:000 rcode: 0000
@@ -66,14 +62,11 @@ class nameServer(socketserver.BaseRequestHandler):
 		_classify = 1
 		_ttl = 5000
 		_datalength = 4
-		res += self.qu
+		res += query_info
 		res += struct.pack('>HHHLH', _name, _type, _classify, _ttl, _datalength)
 		s = ip.split('.')
 		res += struct.pack('BBBB', int(s[0]), int(s[1]), int(s[2]), int(s[3]))
 		return res
-
-	def requestRemote(self):
-		pass
 
 
 	def __composeData(self, header, data):
@@ -87,67 +80,66 @@ class nameServer(socketserver.BaseRequestHandler):
 		dns_tuple = struct.unpack('>HHHHHH', data[0:12])
 		(_id, _flags, _quests, _answers, _author, _addition) = dns_tuple
 		_header = dict(id=_id, flags=_flags, quests=_quests, answers=_answers, author=_author, addition=_addition)
+		if _quests > 1:
+			print(_header)
+			
 		print(_header)
 		pos = 12
 		query_list = []
 		for i in range(_quests):
-			url = ''
-			# get url
-			while True:
-				num = data[pos]
-				print(num)
-				if num == 0:
-					self.qu = data[12:]
-					pos += 1
-					break
-				url_str = data[pos + 1: pos + 1 + num]
-				url_str = ''.join([chr(x) for x in url_str])
-				url += '.' + url_str
-				pos = pos + 1 + num
-			url = self.__geturl(data, pos)
+			url, pos = self.__geturl(data, pos)
+			print('pos', pos)
+			print('geturl:', url)
 			q_type, q_class = struct.unpack('>HH', data[pos:pos + 4])
 			pos += 4
 			
 			query_list.append(dict(url=url, q_type = q_type, q_class = q_class))
+		query_info = data[12:pos]
 		for i in range(_answers):
 			print('pos:',pos)
 			print('now',bin(data[pos]))
 			if data[pos] & 0b11000000 == 0b11000000:
 				print('tiao')
 				pos += 2
-			r_type, r_class, r_TTL, r_DataLength = struct.unpack('>HHLH', data[pos:pos + 10])
-			if r_type == 1:
-				pass
-				
 			else:
-				pos = pos + 10 + r_DataLength
-			
-
+				_, pos = self.__geturl(data, pos)
+			r_type, r_class, r_TTL, r_DataLength = struct.unpack('>HHLH', data[pos:pos + 10])
+			print(r_type, r_class, r_TTL, r_DataLength)
+			pos += 10
+			print('r_type:', r_type)
+			if r_type == 1:
+				ip_list = struct.unpack('BBBB', data[pos: pos + r_DataLength])
+				ip_list = list(map(str, ip_list))
+				ip = '.'.join(ip_list)
+				self.url2ip[url] = ip
+				
+			pos = pos + r_DataLength
 			print('reserive:', r_type, r_class, r_TTL, r_DataLength)
-
 			
 		for i in range(_author):
 			pass
 		for i in range(_addition):
 			pass
 
-		return dict(query_list = query_list, header = _header)
+		return dict(query_list = query_list, header = _header, query_info = query_info)
 	
 	def __geturl(self, data, pos):
 		url = ''
 		# get url
 		while True:
+			print('pos:', pos)
+			if pos > len(data):
+				exit()
 			num = data[pos]
-			print(num)
+			
 			if num == 0:
-				self.qu = data[12:]
 				pos += 1
 				break
 			url_str = data[pos + 1: pos + 1 + num]
 			url_str = ''.join([chr(x) for x in url_str])
 			url += '.' + url_str
 			pos = pos + 1 + num
-		return url.strip('.')
+		return url.strip('.'), pos
 		#return dict(header=_header, queryInfo=DNSQueryDestructor(data[12:]))
 
 	def load_file(self, file_path = './dnsrelay.txt'):
@@ -158,20 +150,4 @@ class nameServer(socketserver.BaseRequestHandler):
 					break
 				ip, name = line.strip().split()
 				self.url2ip[name] = ip
-
-	# def listen(self, data):
-	# 	(ip, das) = self.__parseData(data)
-
-	# 	if ip == '0.0.0.0':
-	# 		pass
-	# 		#self.send()
-	# 	#else if Form[ip] != None:
-	# 	#	pass
-	# 		#self.send(Form[ip])
-	# 	else:
-	# 		pass
-	# 		domain = self.__getFromRemote()
-	# 		#send()
-
-	
 
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import dns.resolver
 
-domain = 'www.qq.com'
+domain = 'www.bupt.com'
 # 74.125.230.127 alt4.stun.l.google.com
 my_resolver = dns.resolver.Resolver()
 my_resolver.nameservers = ['127.0.0.1']
 
 A= my_resolver.query(domain,'A')
-print(A.response)
+#print(A.response)
 for i in A.response.answer:
-	print(i)
+	#print(i)
 	for j in i.items:
 		if j.rdtype == 1:
 			print (j.address)
+
+
+A= my_resolver.query('www.360.com','A')
+A= my_resolver.query('www.qq.com','A')
+A= my_resolver.query('www.sina.com','A')
+A= my_resolver.query('www.360.com','A')
